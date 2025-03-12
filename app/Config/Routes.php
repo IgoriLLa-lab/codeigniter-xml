7a859b6fb240b5a66731047fb2b328dbb7fb95c2
igor <crocusik@mail.ru>
2025-03-12 15:57:35 +0100

    add routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'ContractController::index');
+$routes->get('/contracts/create', 'ContractController::create');
+$routes->post('/contracts/store', 'ContractController::store');
+$routes->get('/contracts/edit/(:num)', 'ContractController::edit/$1');
+$routes->post('/contracts/update/(:num)', 'ContractController::update/$1');
+$routes->post('/contracts/delete/(:num)', 'ContractController::delete/$1');
+$routes->get('/contracts/download/(:num)', 'ContractController::download/$1');
